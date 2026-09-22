a7e3ad9e588fe070b3c463ce48530c32f21faf92
Deepu Sharma <niikhiil.official@gmail.com>
2026-09-22 20:13:38 +0530

    feat: mount app theme provider

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/context/AuthContext';
+import { ThemeProvider } from './src/context/ThemeContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <KeyboardProvider>
-          <AuthProvider>
-          <StatusBar style="auto" />
-          <AppNavigator />
-          </AuthProvider>
-        </KeyboardProvider>
+        <ThemeProvider>
+          <KeyboardProvider>
+            <AuthProvider>
+              <StatusBar style="auto" />
+              <AppNavigator />
+            </AuthProvider>
+          </KeyboardProvider>
+        </ThemeProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
