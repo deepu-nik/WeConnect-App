4b0a633b35aba38987296776a427afc400482c8a
Deepu Sharma <niikhiil.official@gmail.com>
2026-09-22 22:02:09 +0530

    fix: remove unused keyboard-controller provider dependency

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
@@ -12,12 +11,10 @@ export default function App() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <ThemeProvider>
-          <KeyboardProvider>
-            <AuthProvider>
-              <StatusBar style="auto" />
-              <AppNavigator />
-            </AuthProvider>
-          </KeyboardProvider>
+          <AuthProvider>
+            <StatusBar style="auto" />
+            <AppNavigator />
+          </AuthProvider>
         </ThemeProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
